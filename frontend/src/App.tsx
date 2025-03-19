cff2d929f723499ed50eef140f4dc107365c97b5
bensonkimm <benson02@byu.edu>
2025-03-19 13:23:22 -0600

    2nd round

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
+import BottomNavigation from "./BottomNavigation";
+
 
 function App() {
   return (
     <div className="app-container">
       <HomeScreen />
+      {/* Bottom Navigation */}
+      <BottomNavigation />
     </div>
   );
 }
