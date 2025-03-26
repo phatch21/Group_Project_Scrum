01435d7bcbe92e3681428a4027225343f6da3b73
phatch21 <phatch21@byu.edu>
2025-03-26 15:47:56 -0600

    stat.tsx

--- frontend/src/stats.tsx
+++ frontend/src/stats.tsx
@@ -1,21 +1,15 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./GoalsMenu.css";
 
-const GoalsMenu: React.FC = () => {
-  const navigate = useNavigate();
-
+function Stats() {
   return (
     <div className="container">
       <div className="menu-item">Current Goals</div>
       <hr />
-      <div onClick={() => navigate("/GoalInput")} className="menu-item">
-        New Goal
-      </div>
+      <div className="menu-item">New Goal</div>
       <hr />
       <div className="menu-item">Stats</div>
     </div>
   );
-};
+}
 
-export default GoalsMenu;
+export default Stats;
