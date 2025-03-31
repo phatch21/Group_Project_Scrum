ca26f88ccd1adebeeddc66b777503798c03b1346
emmahelquist <154380901+emmahelquist@users.noreply.github.com>
2025-03-31 11:47:28 -0600

    Added everything into pages

---
+++ frontend/src/pages/OrchardPage.tsx
@@ -0,0 +1,13 @@
+import BottomNavigation from "../BottomNavigation";
+import Orchard from "../Orchard";
+
+function OrchardPage() {
+    return (
+        <>
+        <Orchard />
+        <BottomNavigation />
+        </>
+    );
+}
+
+export default OrchardPage;
